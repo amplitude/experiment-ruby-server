ac6fb1fc6c97728d5a6b903d8118c1c5bafbbc41
Tim Yiu <tim.yiu@Tims-MacBook-Pro.local>
2023-09-20 16:33:30 -0700

    fix: update AmplitudeCookie util to work with Analytics Browser 2.0 cookie format

--- lib/experiment/cookie.rb
+++ lib/experiment/cookie.rb
@@ -1,4 +1,5 @@
 require 'base64'
+require 'json'
 
 module AmplitudeExperiment
   # This class provides utility functions for parsing and handling identity from Amplitude cookies.
@@ -7,8 +8,15 @@ module AmplitudeExperiment
     #
     # @param [String] api_key The Amplitude API Key
     # @return [String] The cookie name that Amplitude sets for the provided
-    def self.cookie_name(api_key)
-      raise ArgumentError, 'Invalid Amplitude API Key' if api_key.nil? || api_key.length < 6
+    def self.cookie_name(api_key, new: false)
+      raise ArgumentError, 'Invalid Amplitude API Key' if api_key.nil?
+
+      if new
+        raise ArgumentError, 'Invalid Amplitude API Key' if api_key.length < 10
+
+        return "AMP_#{api_key[0..9]}"
+      end
+      raise ArgumentError, 'Invalid Amplitude API Key' if api_key.length < 6
 
       "amp_#{api_key[0..5]}"
     end
@@ -17,7 +25,17 @@ module AmplitudeExperiment
     #
     # @param [String] amplitude_cookie A string from the amplitude cookie
     # @return [User] a Experiment User context containing a device_id and user_id
-    def self.parse(amplitude_cookie)
+    def self.parse(amplitude_cookie, new: false)
+      if new
+        begin
+          decoding = Base64.decode64(amplitude_cookie).force_encoding('UTF-8')
+          json_data = URI.decode_www_form_component(decoding)
+          user_session_hash = JSON.parse(json_data)
+          return User.new(user_id: user_session_hash['userId'], device_id: user_session_hash['deviceId'])
+        rescue StandardError
+          return User.new()
+        end
+      end
       values = amplitude_cookie.split('.', -1)
       user_id = nil
       unless values[1].nil? || values[1].empty?
@@ -34,8 +52,15 @@ module AmplitudeExperiment
     #
     # @param [String] device_id A device id to set
     # @return [String] A cookie string to set for the Amplitude Javascript SDK to read
-    def self.generate(device_id)
-      "#{device_id}.........."
+    def self.generate(device_id, new: false)
+      return "#{device_id}.........." unless new
+
+      user_session_hash = {
+        'deviceId' => device_id
+      }
+      json_data = JSON.generate(user_session_hash)
+      encoded_json = URI.encode_www_form_component(json_data)
+      Base64.strict_encode64(encoded_json)
     end
   end
 end
--- spec/experiment/remote/cookie_spec.rb
+++ spec/experiment/remote/cookie_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 module AmplitudeExperiment
   describe AmplitudeCookie do
+    before(:each) do
+      @user_and_device_new_cookie = 'JTdCJTIydXNlcklkJTIyJTNBJTIydGVzdCU0MGFtcGxpdHVkZS5jb20lMjIlMkMlMjJkZXZpY2VJZCUyMiUzQSUyMmRldmljZUlkJTIyJTdE'
+      @device_new_cookie = 'JTdCJTIyZGV2aWNlSWQlMjIlM0ElMjJkZXZpY2VJZCUyMiU3RA=='
+    end
+
     describe '#cookie_name' do
       it 'test invalid api key throw error' do
         expect { AmplitudeCookie.cookie_name('') }.to raise_error(ArgumentError)
@@ -9,6 +14,7 @@ module AmplitudeExperiment
 
       it 'test valid api key return cookie name' do
         expect(AmplitudeCookie.cookie_name('1234567')).to eq('amp_123456')
+        expect(AmplitudeCookie.cookie_name('1234567890', new: true)).to eq('AMP_1234567890')
       end
     end
 
@@ -17,10 +23,16 @@ module AmplitudeExperiment
         user = AmplitudeCookie.parse('deviceId...1f1gkeib1.1f1gkeib1.dv.1ir.20q')
         expect(user.device_id).to eq('deviceId')
         expect(user.user_id).to be_nil
+        user = AmplitudeCookie.parse(@device_new_cookie, new: true)
+        expect(user.device_id).to eq('deviceId')
+        expect(user.user_id).to be_nil
       end
 
       it 'test parse cookie with device id and user id' do
         user = AmplitudeCookie.parse('deviceId.dGVzdEBhbXBsaXR1ZGUuY29t..1f1gkeib1.1f1gkeib1.dv.1ir.20q')
+        expect(user.device_id).to eq('deviceId')
+        expect(user.user_id).to eq('test@amplitude.com')
+        user = AmplitudeCookie.parse(@user_and_device_new_cookie, new: true)
         expect(user.device_id).to eq('deviceId')
         expect(user.user_id).to eq('test@amplitude.com')
       end
@@ -36,13 +48,18 @@ module AmplitudeExperiment
         user = AmplitudeCookie.parse('deviceId.Y8O3Pg==..1f1gkeib1.1f1gkeib1.dv.1ir.20q')
         expect(user.device_id).to eq('deviceId')
         expect(user.user_id).to be_nil
+        user = AmplitudeCookie.parse(@user_and_device_new_cookie, new: true)
+        expect(user.device_id).to be_nil
+        expect(user.user_id).to be_nil
       end
     end
 
     describe '#generate' do
       it 'test generate' do
-        result = AmplitudeCookie.generate('deviceId')
-        expect(result).to eq('deviceId..........')
+        old = AmplitudeCookie.generate('deviceId')
+        expect(old).to eq('deviceId..........')
+        new = AmplitudeCookie.generate('deviceId', new: true)
+        expect(new).to eq(@device_new_cookie)
       end
     end
   end
