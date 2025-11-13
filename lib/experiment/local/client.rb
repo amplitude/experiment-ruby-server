7dd752935e231be6380b52326500478f0a836a24
Peter Zhu <peter.zhu@amplitude.com>
2025-11-13 05:23:59 -0800

    fix: check options

--- lib/experiment/local/client.rb
+++ lib/experiment/local/client.rb
@@ -84,7 +84,9 @@ module AmplitudeExperiment
       result = @engine.evaluate(context, sorted_flags)
       @logger.debug("[Experiment] evaluate - result: #{result}") if @config.debug
       variants = AmplitudeExperiment.evaluation_variants_json_to_variants(result)
-      @exposure_service.track(Exposure.new(user, variants)) if options.tracks_exposure == true
+      if options.tracks_exposure == true
+        @exposure_service.track(Exposure.new(user, variants))
+      end
       # @deprecated Assignment tracking is deprecated. Use ExposureService with Exposure tracking instead.
       @assignment_service&.track(Assignment.new(user, variants))
       variants
