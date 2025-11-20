ea5b6e023f00bf341edc0714843309e61619331b
Peter Zhu <peter.zhu@amplitude.com>
2025-11-19 16:08:31 -0800

    docs: add comments

--- lib/experiment/remote/fetch_options.rb
+++ lib/experiment/remote/fetch_options.rb
@@ -2,10 +2,12 @@ module AmplitudeExperiment
   # Fetch options
   class FetchOptions
     # Whether to track assignment events.
+    # If not provided, the default is null, which will use server default (to track assignment events).
     # @return [Boolean, nil] the value of tracks_assignment
     attr_accessor :tracks_assignment
 
     # Whether to track exposure events.
+    # If not provided, the default is null, which will use server default (to not track exposure events).
     # @return [Boolean, nil] the value of tracks_exposure
     attr_accessor :tracks_exposure
 
